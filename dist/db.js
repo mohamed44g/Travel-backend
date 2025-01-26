27982501704b482f0b059eb9ede5af420ca4bb9a
Mohamed badry <85965151+mohamed44g@users.noreply.github.com>
2025-01-26 08:28:18 +0200

    Update db.js

--- dist/db.js
+++ dist/db.js
@@ -12,7 +12,6 @@ dotenv.config();
 //     require: true,
 //   },
 // });
-console.log(process.env.DATABASE_URL)
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: true,
